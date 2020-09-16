a162ef4f90dbb48ec739ae484df236b5fcdc0b54
Michael Lore <mlore95@gmail.com>
2020-09-16 14:51:32 -0400

    import header on message_sent

---
+++ message_sent.php
@@ -0,0 +1,30 @@
+<?php 
+  include "header.php"; 
+?>
+    <!-- Section A: Thanks Message -->
+    <section id="thanks" class="text-center py-3">
+        <div class="container">
+            <h2 class="section-title">Thanks!</h2>
+            <div class="bottom-line"></div>
+            <p class="lead">Your message was sent. I'll get back to you as soon as possible</p>
+            <p>If you haven't already, <a href="work.html">check out some of my projects</a> or <a href="about.html">learn more about me.</a></p>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="main-footer">
+        <div class="footer-content container">
+            <p>Copyright &copy; 2020 All Rights Reserved</p>
+            <div class="social">
+                <a href="mailto:mlore95@gmail.com" target="_blank" rel="noopener noreferrer"><i
+                        class="fas fa-envelope fa-2x"></i></a>
+                <a href="https://www.linkedin.com/in/mlore/" target="_blank" rel="noopener noreferrer"><i
+                        class="fab fa-linkedin fa-2x"></i></a>
+                <a href="https://github.com/michaellore" target="_blank" rel="noopener noreferrer"><i
+                        class="fab fa-github-square fa-2x"></i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
